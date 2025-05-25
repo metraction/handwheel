1a4d85b202552c245f3e44453260bd56062b5a65
Marijus <spastai@gmail.com>
2025-05-25 13:22:39 +0200

    Fetch tags as well

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -8,7 +8,7 @@ VERSION=$(echo "$DESCRIBE" | sed -E 's/^((v[0-9]+\.[0-9]+\.[0-9]+)).*/\1/')
 if [[ ! "$VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   VERSION="v0.0.0"
 fi
-COMMITS=$(echo "$DESCRIBE" | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
+DISTANCE=$(echo "$DESCRIBE" | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
 SHORT_COMMIT=$(echo "$DESCRIBE" | sed -E 's/.*-g([0-9a-f]+)$/\1/')
 
 # Get the current branch
@@ -19,11 +19,11 @@ BRANCH_ALNUM=$(echo "$CURRENT_BRANCH" | tr -cd '[:alnum:]')
 
 # Print version based on branch
 if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
-    if [ "$COMMITS" = "0" ] || [ -z "$COMMITS" ]; then
+    if [ "$DISTANCE" = "0" ] || [ -z "$DISTANCE" ]; then
         echo $VERSION
     else
-        echo $VERSION-$COMMITS.g$SHORT_COMMIT
+        echo $VERSION-$DISTANCE.g$SHORT_COMMIT
     fi
 else
-    echo ${VERSION}-${BRANCH_ALNUM}-${COMMITS}+${SHORT_COMMIT}
+    echo ${VERSION}-${BRANCH_ALNUM}-${DISTANCE}+${SHORT_COMMIT}
 fi
