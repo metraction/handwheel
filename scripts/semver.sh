c072d03bb032a773c7e5e83d211069f59dbf190b
Marijus <spastai@gmail.com>
2025-05-25 20:12:18 +0200

    Use patch version

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -14,25 +14,25 @@ SHORT_COMMIT=$(echo "$DESCRIBE" | sed -E 's/.*-g([0-9a-f]+)$/\1/')
 # Get the current branch
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-# Remove non-alphanumeric chars from branch name
-BRANCH_ALNUM=$(echo "$CURRENT_BRANCH" | tr -cd '[:alnum:]')
-
-# Print version based on branch or tag
+# If it is tag - use it
 if [ "$CURRENT_BRANCH" = "HEAD" ]; then
     # On tag, output pure semver
     echo $VERSION
-elif [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
-    if [ "$DISTANCE" = "0" ] || [ -z "$DISTANCE" ]; then
-        echo $VERSION
-    else
-        echo $VERSION-$DISTANCE+$SHORT_COMMIT
-    fi
 else
-    # Increment patch version by 1
-    PATCH=$(echo $VERSION | sed -E 's/^v?([0-9]+)\.([0-9]+)\.([0-9]+)$/\3/')
-    PATCH=$((PATCH + 1))
+    # Use distance as patch version on all branches
     MAJOR=$(echo $VERSION | sed -E 's/^v?([0-9]+)\..*/\1/')
     MINOR=$(echo $VERSION | sed -E 's/^v?[0-9]+\.([0-9]+)\..*/\1/')
-    NEW_VERSION="$MAJOR.$MINOR.$PATCH"
-    echo ${NEW_VERSION}-${BRANCH_ALNUM}-${DISTANCE}+$SHORT_COMMIT
+    PATCH=$DISTANCE
+    if [ -z "$PATCH" ] || [ "$PATCH" = "" ]; then
+        PATCH=0
+    fi
+    if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
+        # If it is default branch - use distance as patch version
+        echo "$MAJOR.$MINOR.$PATCH+$SHORT_COMMIT"
+    else
+        # If it is not default branch - increment patch version by 1 and use branch name as RC
+        PATCH=$(($PATCH + 1))
+        BRANCH_ALNUM=$(echo "$CURRENT_BRANCH" | tr -cd '[:alnum:]')
+        echo "$MAJOR.$MINOR.$PATCH-$BRANCH_ALNUM+$SHORT_COMMIT"
+    fi
 fi
