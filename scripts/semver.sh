8d33ed7e18bfdd26ada82b3320aa1529df7a15a3
Marijus <spastai@gmail.com>
2025-05-25 13:11:53 +0200

    Set semver

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-DEFAULT_BRANCH=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
-# Get the latest tag
-LATEST_TAG=$(git describe --tags --abbrev=0)
-
-# Get the latest tag's version number
-VERSION=$(echo $LATEST_TAG | grep -oP 'v\d+\.\d+\.\d+')
-
-# Get the latest tag's commit hash
-COMMIT=$(git rev-list -n 1 $LATEST_TAG)
-
-# Get the number of commits since the latest tag
-COMMITS=$(git rev-list --count $COMMIT..HEAD)
+DEFAULT_BRANCH=main
+# Get describe info
+DESCRIBE=$(git describe --tags --long --always)
+# Example: v1.2.3-5-gabc1234
+VERSION=$(echo "$DESCRIBE" | sed -E 's/^((v[0-9]+\.[0-9]+\.[0-9]+)).*/\1/')
+COMMITS=$(echo "$DESCRIBE" | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
+SHORT_COMMIT=$(echo "$DESCRIBE" | sed -E 's/.*-g([0-9a-f]+)$/\1/')
 
 # Get the current branch
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -21,11 +16,11 @@ BRANCH_ALNUM=$(echo "$CURRENT_BRANCH" | tr -cd '[:alnum:]')
 
 # Print version based on branch
 if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
-    if [ $COMMITS -eq 0 ]; then
+    if [ "$COMMITS" = "0" ] || [ -z "$COMMITS" ]; then
         echo $VERSION
     else
-        echo $VERSION-$COMMITS.g$(git rev-parse --short HEAD)
+        echo $VERSION-$COMMITS.g$SHORT_COMMIT
     fi
 else
-    echo ${VERSION}-${BRANCH_ALNUM}-${COMMITS}+$(git rev-parse --short HEAD)
+    echo ${VERSION}-${BRANCH_ALNUM}-${COMMITS}+${SHORT_COMMIT}
 fi
