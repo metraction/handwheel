24e15f8903b65d20596f74cc3149d0220de237f2
spastai <spastai@gmail.com>
2025-05-25 18:03:43 +0200

    Set dynamic badges (#3)
    * Add dynamic badges
    
    * Add script for semver
    
    * Set semver
    
    * Trigger build
    
    * Build docker as well
    
    * Show version
    
    * Use default version
    
    * Fetch tags as well
    
    * Remove branch action
    
    * Remove plust
    
    * Check branch
    
    * Modify tag
    
    * Remove +
    
    * Change in helm as well
    
    * Remove short sha
    
    * Remove image tag
    
    * Bring back short version

---
+++ scripts/semver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DEFAULT_BRANCH=main
+# Get describe info
+DESCRIBE=$(git describe --tags --long --always)
+# Example: v1.2.3-5-gabc1234
+VERSION=$(echo "$DESCRIBE" | sed -E 's/^((v[0-9]+\.[0-9]+\.[0-9]+)).*/\1/')
+if [[ ! "$VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  VERSION="v0.0.0"
+fi
+DISTANCE=$(echo "$DESCRIBE" | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
+SHORT_COMMIT=$(echo "$DESCRIBE" | sed -E 's/.*-g([0-9a-f]+)$/\1/')
+
+# Get the current branch
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Remove non-alphanumeric chars from branch name
+BRANCH_ALNUM=$(echo "$CURRENT_BRANCH" | tr -cd '[:alnum:]')
+
+# Print version based on branch
+if [ "$CURRENT_BRANCH" = "$DEFAULT_BRANCH" ]; then
+    if [ "$DISTANCE" = "0" ] || [ -z "$DISTANCE" ]; then
+        echo $VERSION
+    else
+        echo $VERSION-$DISTANCE+$SHORT_COMMIT
+    fi
+else
+    echo ${VERSION}-${BRANCH_ALNUM}-${DISTANCE}+$SHORT_COMMIT
+fi
