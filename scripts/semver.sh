3915848ae4bb6c881740b3dd7f32cd479883e1b1
Marijus <spastai@gmail.com>
2025-05-25 18:20:09 +0200

    Remove v

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -4,11 +4,11 @@ DEFAULT_BRANCH=main
 # Get describe info
 DESCRIBE=$(git describe --tags --long --always)
 # Example: v1.2.3-5-gabc1234
-VERSION=$(echo "$DESCRIBE" | sed -E 's/^((v[0-9]+\.[0-9]+\.[0-9]+)).*/\1/')
-if [[ ! "$VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  VERSION="v0.0.0"
+VERSION=$(echo "$DESCRIBE" | sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  VERSION="0.0.0"
 fi
-DISTANCE=$(echo "$DESCRIBE" | sed -E 's/^v[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
+DISTANCE=$(echo "$DESCRIBE" | sed -E 's/^v?[0-9]+\.[0-9]+\.[0-9]+-([0-9]+)-g[0-9a-f]+$/\1/')
 SHORT_COMMIT=$(echo "$DESCRIBE" | sed -E 's/.*-g([0-9a-f]+)$/\1/')
 
 # Get the current branch
